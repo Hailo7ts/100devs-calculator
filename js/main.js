ded124dc16faeb6f094e75c7e8c74ba0a49574d3
Hailey Martin <hailo7ts@gmail.com>
2024-05-25 15:58:29 -0600

    start of calc

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+//calc constructor
+
+let total = 0
+
+let operation = document.querySelector('.operation').addEventListener()
+
+
+function Calculator(add, sub, div, multi, equal){
+    this.add = add
+    this.subtract = sub
+    this.divide = div 
+    this.multiply = multi 
+    this.equal = equal
+
+    this.addition = function(){
+
+    }
+}
